f169267adce572668f78300cf51f2f0fca6bc639
johneckert <johnteckert@gmail.com>
2020-02-11 15:01:50 -0500

    new branch

--- build-handler.ps1
+++ build-handler.ps1
@@ -2,4 +2,4 @@ write-host "I am in build-handler.ps1 in buildtester" -ForegroundColor Cyan
 
 New-Item -Path . -Name "workspace" -ItemType "directory"
 
-New-Item -Path './workspace' -Name "built3"-ItemType "file" -Value "From Build Tester"
+New-Item -Path './workspace' -Name "built3dada"-ItemType "file" -Value "From Build Tester"

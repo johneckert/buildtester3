63d02156a01007b572b3f49d6c7aacf83c3750c8
John Eckert <johnteckert@gmail.com>
2020-02-13 16:20:22 -0500

    add naming changes

--- build-handler.ps1
+++
@@ -1,7 +0,0 @@
-write-host "I am in build-handler.ps1 in buildtester" -ForegroundColor Cyan
-
-New-Item -Path . -Name "workspace" -ItemType "directory"
-
-New-Item -Path './workspace' -Name "built3dada"-ItemType "file" -Value "From Build Tester"
-
-New-Item -Path './workspace' -Name "built2log"-ItemType "file" -Value "this would be log data about the project build."
